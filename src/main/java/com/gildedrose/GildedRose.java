3177dc028220d9bb6e8521512370061cc14c222a
Quentin ­­ <quentin.lagier34@orange.fr>
2020-10-29 18:35:55 +0100

    modification méthode incrementQuality et des constantes (pour correspondre au specs)

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -6,8 +6,8 @@ class GildedRose {
   static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
 
   static final int QUALITY_MAX_VALUE = 50;
-  static final int BACKSTAGE_PASSES_SELLIN_LIMIT2 = 11;
-  static final int BACKSTAGE_PASSES_SELLIN_LIMIT3 = 6;
+  static final int BACKSTAGE_PASSES_SELLIN_LIMIT2 = 10;
+  static final int BACKSTAGE_PASSES_SELLIN_LIMIT3 = 5;
 
   transient Item[] items;
 
@@ -15,21 +15,23 @@ class GildedRose {
     this.items = itemsList;
   }
 
-  private void incrementQuality(Item item) {
-    if (item.quality < QUALITY_MAX_VALUE) {
-      item.quality = item.quality + 1;
+  private void incrementQuality(Item item, int n) {
+    if (item.quality + n <= QUALITY_MAX_VALUE) {
+      item.quality += n;
+    } else {
+      item.quality = QUALITY_MAX_VALUE;
     }
   }
 
   private void decrementQuality(Item item) {
     if (!item.name.equals(SULFURAS) && (item.quality > 0)) {
-      item.quality = item.quality - 1;
+      item.quality -= 1;
     }
   }
 
   private void decrementSellIn(Item item) {
     if (!item.name.equals(SULFURAS)) {
-      item.sellIn = item.sellIn - 1;
+      item.sellIn -= 1;
     }
   }
 
@@ -39,20 +41,16 @@ class GildedRose {
     // parcours de tous les items
       if (item.name.equals(AGED_BRIE)) {
         // si c'est l'item "Agent Brie" ou l'item "Backstage passes"
-        incrementQuality(item);
+        incrementQuality(item, 1);
 
       } else if (item.name.equals(BACKSTAGE_PASSES)) {
-        // si c'est l'item "Backstage passes"
-        incrementQuality(item);
-
         // la qualité augmente de 2 quand il reste 10 jours ou moins
-        if (item.sellIn < BACKSTAGE_PASSES_SELLIN_LIMIT2) {
-          incrementQuality(item);
-        }
-
-        // et de 3 quand il reste 5 jours ou moins (mais la qualité tombe à 0 après le concert)
-        if (item.sellIn < BACKSTAGE_PASSES_SELLIN_LIMIT3) {
-          incrementQuality(item);
+        if (item.sellIn <= BACKSTAGE_PASSES_SELLIN_LIMIT3) {
+          incrementQuality(item, 3);
+        } else if (item.sellIn <= BACKSTAGE_PASSES_SELLIN_LIMIT2) {
+          incrementQuality(item, 2);
+        } else {
+           incrementQuality(item, 1);
         }
       } else {
         // si ce n'est ni l'item "Agent Brie" ni l'item "Backstage passes"
@@ -63,7 +61,7 @@ class GildedRose {
 
       if (item.sellIn < 0) {
         if (item.name.equals(AGED_BRIE)) {
-          incrementQuality(item);
+          incrementQuality(item, 1);
           // ici pour le Aged Brie périmé elle est incrémentée une 2e fois
         } else {
           if (item.name.equals(BACKSTAGE_PASSES)) {
